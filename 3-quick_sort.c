7bf6a39e07e1863c5fe3d6ff6752f0b71d4a165d
meisibley <mkong99@yahoo.com>
2023-08-14 16:44:22 -0700

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * quick_sort - It sorts an array of int in ascending order using Quick sort
+ * @array: Given array need to sort
+ * @size: size of this array
+ *
+ * Return: nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+	int i, j;
+
+	if(!array || size <= 1)
+		return;
+	i = 0;
+	j = (int)size - 1;
+	quicksort(array, i, j, size);
+}
+
+/**
+ * quicksort - recursive to run pivot tree, print array each branch created
+ * @array: Given array need to sort
+ * @low: low index in the array (i)
+ * @high: high index in the array (j)
+ * @size: array size
+ *
+ * Return: nothing
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int p;
+
+	if (low < high)
+	{
+		p = partition(array, low, high, size);
+		quicksort(array, low, p - 1, size);
+		quicksort(array, p + 1, high, size);
+	}
+}
+
+/**
+ * partition - Lomuto scheme partition, sort array
+ * @array: Given array
+ * @low: low index in the array
+ * @high: high index in the array
+ * @size: array size
+ *
+ * Return: next pivot index
+ */
+size_t partition(int *array, int low, int high, size_t size)
+{
+	int i, j, pivot, swap;
+
+	pivot = array[high];
+	i = low - 1;
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap = array[j];
+			array[j] = array[i];
+			array[i] = swap;
+			if (i != j)
+				print_array(array, size);
+		}
+	}
+	i++;
+	swap = array[high];
+	array[high] = array[i];
+	array[i] = swap;
+	if (i != high)
+		print_array(array, size);
+	return (i);
+}
