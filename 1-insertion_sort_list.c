0c763a2a0f987f9e171a4e56501d55f7097f685c
meisibley <mkong99@yahoo.com>
2023-08-14 08:52:06 -0700

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - insertion sort a doubly linked list
+ * @list: the doubly linked list
+ *
+ * Return: nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = NULL, *pnode = NULL, *node = NULL, *nnode = NULL;
+
+	if (list == NULL)
+		return;
+	head = *list, node = head;
+	while (node)
+	{
+		pnode = node->prev;
+		while (pnode && pnode->n > node->n)
+		{
+			nnode = node->next;
+			if (pnode->prev)
+				pnode->prev->next = node;
+			node->prev = pnode->prev;
+			pnode->next = nnode;
+			pnode->prev = node;
+			if (nnode)
+				nnode->prev = pnode;
+			node->next = pnode;
+
+			pnode = node->prev;
+			if (head->prev)
+			head = head->prev;	
+			print_list(head);
+		}
+		node = node->next;
+	}
+	*list = head;
+}
